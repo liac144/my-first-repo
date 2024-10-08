2f74de5edfd508412d5c8040ad04caf9ae55d9ea
liac144 <chernrui@myuax.com>
2024-10-08 11:57:03 +0200

    Add files via upload

---
+++ ke.py
@@ -0,0 +1,9 @@
+#Calculate Kinetic Energy
+print ("We're goint to calculate the kinetic energy of an objet with its mass and its velocity")
+mass = float (input("Enter the masss (in kilograms):"))
+velocity = float (input("Enter the velocity (in m/s):"))
+print("The2se numbers are the mass and the velocity", mass, velocity)
+def conversion (mass, velocity):
+  kinetic_energy = ((0.5)*(mass)*((velocity)**2))
+kinetic_energy = ((0.5)*(mass)*((velocity)**2))
+print ("Now we have the kenetic energy", kinetic_energy )
