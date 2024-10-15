fefbe3d4510521485a4fdaec2248a4859c9815bc
liac144 <chernrui@myuax.com>
2024-10-15 09:21:33 +0200

    Add files via upload

---
+++ 15102024.py
@@ -0,0 +1,6 @@
+# Join the reversed words back into a sentence
+sentence = input("Please enter a sentence: ")
+words = sentence.split()
+reversed_words = words[::-1]
+reversed_sentence = ' '.join(reversed_words)
+print("Reversed sentence:", reversed_sentence)
